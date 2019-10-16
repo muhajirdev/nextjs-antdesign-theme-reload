9aea44a38f00bf616beb345cdeba78472e33c1b7
Muhammad Muhajir <muhajirframe@gmail.com>
2019-10-16 11:09:41 +0700

    support hot reload

--- next.config.js
+++ next.config.js
@@ -1,40 +1,36 @@
 /* eslint-disable */
-const withLess = require('@zeit/next-less')
-const lessToJS = require('less-vars-to-js')
-const fs = require('fs')
-const path = require('path')
-
-// Where your antd-custom.less file lives
-const themeVariables = lessToJS(
-  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
-)
+const withLess = require("@zeit/next-less");
+const LessThemePlugin = require("webpack-less-theme-plugin");
 
 module.exports = withLess({
   lessLoaderOptions: {
-    javascriptEnabled: true,
-    modifyVars: themeVariables, // make your antd custom effective
+    javascriptEnabled: true
   },
   webpack: (config, { isServer }) => {
+    config.plugins.push(
+      new LessThemePlugin({ theme: "./assets/antd-custom.less" })
+    );
+
     if (isServer) {
-      const antStyles = /antd\/.*?\/style.*?/
-      const origExternals = [...config.externals]
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
       config.externals = [
         (context, request, callback) => {
-          if (request.match(antStyles)) return callback()
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback)
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === "function") {
+            origExternals[0](context, request, callback);
           } else {
-            callback()
+            callback();
           }
         },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ]
+        ...(typeof origExternals[0] === "function" ? [] : origExternals)
+      ];
 
       config.module.rules.unshift({
         test: antStyles,
-        use: 'null-loader',
-      })
+        use: "null-loader"
+      });
     }
-    return config
-  },
-})
+    return config;
+  }
+});
